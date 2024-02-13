0c6cfe4df9256c156ef1a09fb3899d52799a3b5e
sandy <sandy@floatic.io>
2024-02-13 20:58:40 +0900

    chore: change height 200 to 500 for gs demo

--- src/main/kotlin/com/example/binpacking/entity/Constants.kt
+++ src/main/kotlin/com/example/binpacking/entity/Constants.kt
@@ -10,7 +10,7 @@ class ToteSpec(
     margin: Double = 0.0, // 0.0 ~ 1.0, ex) 0.8
 ) {
     val width: Double = 385 * (1 - margin)
-    val height: Double = 200 * (1 - margin)
+    val height: Double = 500 * (1 - margin)
     val depth: Double = 480 * (1 - margin)
     val weight: Double = 12000 * (1 - margin)
 }
